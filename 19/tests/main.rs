dc2d41806d750ec48cb01a3341028d36866965fe
Irene Knapp <ireneista@gmail.com>
2020-12-21 23:29:34 -0800

    fix tests

--- 19/tests/main.rs
+++ 19/tests/main.rs
@@ -4,10 +4,10 @@ use std::process::Command;
 
 #[test]
 fn personal_input() -> Result<(), Box<dyn std::error::Error>> {
-  let mut command = Command::cargo_bin("advent_17")?;
+  let mut command = Command::cargo_bin("advent_19")?;
 
   command.arg("input");
-  command.assert().success().stdout("265\n1936\n");
+  command.assert().success().stdout("176\n352\n");
 
   Ok(())
 }
